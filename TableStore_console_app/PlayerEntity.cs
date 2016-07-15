a0597dcb153addd921d47fcb86bcb6a19c61c149
James Kenny <james.m.kenny@gmail.com>
2016-07-15 17:22:16 +0100

    Sample Created

---
+++ TableStore_console_app/PlayerEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStore_console_app
+{
+    public class PlayerEntity : TableEntity
+    {
+        public PlayerEntity(string skey, string srow)
+        {
+            this.PartitionKey = skey;
+            this.RowKey = srow;
+        }
+
+        public PlayerEntity() { }
+
+        public string Sport_VC { get; set; }
+        public string Club_VC { get; set; }
+        public string First_Name_VC { get; set; }
+        public string Last_Name_VC { get; set; }
+        public string Position_VC { get; set; }
+    }
+}
--- TableStore_console_app/Program.cs
+++ TableStore_console_app/Program.cs
@@ -3,13 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace TableStore_console_app
 {
     class Program
     {
+        private static string _connectionString = "DefaultEndpointsProtocol=https;AccountName=serversncodedemo;AccountKey=QAQwB44359UPFbjkB+4QpAy+HsZYZF2BT1Fo4OJi+CFhSWJd0HU+X05wMAIgl2LhJ2FSgG9csM8ZRU//scNqeg==";
+
         static void Main(string[] args)
         {
+            //Create a storage account object.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_connectionString);
+            
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("player");
+
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // Get All Players for a sport
+            TableQuery<PlayerEntity> query = new TableQuery<PlayerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "soccer"));
+
+            int iCount = 0;
+            foreach (PlayerEntity entity in table.ExecuteQuery(query))
+            {
+                Console.WriteLine("Club:" + entity.Club_VC + " Player Name: " + entity.First_Name_VC + " " + entity.Last_Name_VC + " Position: " + entity.Position_VC);
+
+                iCount++;
+            }
+
+            CreateData("soccer", iCount.ToString(), "Marcus", "Rashford", "Man Utd", "Striker");
+
+            Console.ReadKey();
+        }
+
+        static Boolean CreateData(string sSport, string sRow, string sFirstName, string sLastName, string sClub, string sPostition)
+        {
+            Boolean bSuccess = false;
+
+            // Create our player
+           
+            // Create the entity with a partition key for sport and a row
+            // Row should be unique within that partition
+            PlayerEntity _record = new PlayerEntity(sSport, sRow);
+
+            _record.Sport_VC = sSport;
+            _record.First_Name_VC = sFirstName;
+            _record.Last_Name_VC = sLastName;
+            _record.Club_VC = sClub;
+            _record.Position_VC = sPostition;
+            
+            //Create a storage account object.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(_connectionString);
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("player");
+
+
+            // Create the TableOperation object that inserts the customer entity.
+            TableOperation insertOperation = TableOperation.Insert(_record);
+
+            try
+            {
+
+                // Execute the insert operation.
+                table.Execute(insertOperation);
+
+                bSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                bSuccess = false;
+            }
+
+
+            return bSuccess;
         }
     }
 }
